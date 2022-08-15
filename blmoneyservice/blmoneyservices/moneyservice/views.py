5eff392e3a7fc3af341793634ec1906486ca326b
Manologk <emmanuelsiame29@gmail.com>
2022-08-15 14:02:17 +0300

    DB working

--- blmoneyservice/blmoneyservices/moneyservice/views.py
+++ blmoneyservice/blmoneyservices/moneyservice/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .moneytransferalgo import Variablez
+from users.models import Client, Transaction, TransactionDetails
+
 import random
 
 
@@ -28,21 +30,28 @@ def homepage(request):
         amountZam_Rus = request.POST['amountzmw']
         resRus_Zam = algo.rub2kwacha(float(amountRus_Zam), usd_Rub_Buying, zmw_usd_selling)
         resZam_Rus = algo.kwacha2rub(float(amountZam_Rus), zmw_usd_buying, usd_Rub_Selling)
+        transaction = Transaction(trans_type="Rus-Zam", amount=amountRus_Zam)
+        transaction.save()
         return redirect(get_details_rus_zam)
+
     return render(request, 'moneyservice/home.html')
 
 
 def get_details_rus_zam(request):
     global amountRus_Zam, resRus_Zam
+    # if request.method == 'POST':
+
+
     return render(request, 'moneyservice/recipientDetailsRUS_ZAM.html', {
         'amount': amountRus_Zam,
         'commission': resRus_Zam[2],
-        'total': resRus_Zam[3],
+        'total': resRus_Zam[3]
     })
 
 
 def display_transaction_rus_zam(request):
     global amountRus_Zam, resRus_Zam
+    print(request.POST)
     return render(request, 'moneyservice/transferRus_Zam.html', {'amount': amountRus_Zam})
 
 
